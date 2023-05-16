54848d4747606d7654962c099aba0fc381d34791
Romain9685 <rom1.grevrand@gmail.com>
2023-05-16 10:09:57 +0100

    header et footer

---
+++ app/view/common/footer.php
@@ -0,0 +1,10 @@
+</main>
+<footer>
+    <ul>
+        <li> <a href="#">Mentions légales</a></li>
+        <li> <a href="#">Politique de confidentialité</a></li>
+        <li> <a href = ""><img src ="public/images/logos/Logo-insta-nb.png"></a><li>
+    </ul>
+</footer>
+</body>
+</html>
