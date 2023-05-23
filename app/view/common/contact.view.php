e427d85da4d9d9fcaec47e40cccd9b8579f1f195
Romain9685 <rom1.grevrand@gmail.com>
2023-05-23 17:45:50 +0100

    contact

---
+++ app/view/common/contact.view.php
@@ -0,0 +1,7 @@
+<form action = ... method="POST">
+<label>Nom : <input type="text" name="nom" required/> </label>
+<label>Prenom : <input type="text" name="prenom" required/> </label>
+<label>objet du mail : <input type="text" name="objet" /> </label>
+<label> Mail : <textarea" name="mail"></textarea></label>
+</form>
+
---
+++ contact.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+require_once "config.php";
+
+if (!empty($_SESSION['message'])) {
+    $message = $_SESSION['message'];
+    unset($_SESSION['message']);
+}
+
+$page_title = 'Contact';
