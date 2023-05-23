937c8ecb270457d80a373e4f5c5e9108739daae3
chattigre <admin@chattigre.fr>
2023-05-23 19:55:01 +0200

    Catalogue v0.2

--- app/view/catalogue.view.php
+++ app/view/catalogue.view.php
@@ -1,7 +1,15 @@
 <div class="page-content">
-    <h1>Notre catalogue</h1>
-    <p>Sur cette page, retrouvez l'ensemble de nos bières. Il ne vous  reste plus qu'à trouver celle que vous souhaitez découvrir en premier !</p>
+    <?php if(!defined("cata_included")):?>
+        <h1>Notre catalogue</h1>
+        <p>Sur cette page, retrouvez l'ensemble de nos bières. Il ne vous  reste plus qu'à trouver celle que vous souhaitez découvrir en premier !</p>
+    <?php endif ?>
     <?php foreach($catalogue as $biere):?>
-        <div class="biere-block"><?=var_dump($biere)?></div>
+        <div class="biere-block">
+            <a href="biere.php?id=<?=$biere['ref_biere']?>">
+                <img src="public/images/bieres/bouteille_<?=$biere['ref_biere']?>.png" alt="<?=$biere['nom']?>">
+                <div class="biere-title"><?=$biere['nom']?></div>
+                <div class="biere-flavour"><?=$biere['ingredients_titre']?></div>
+            </a>
+        </div>
     <?php endforeach?>
 </div>
