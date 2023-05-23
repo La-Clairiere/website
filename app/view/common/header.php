b2abf5e21bd85258fb36b0a9ea234c586cbc126c
chattigre <admin@chattigre.fr>
2023-05-23 19:54:39 +0200

    Update header.php

--- app/view/common/header.php
+++ app/view/common/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Accueil | La Clairiere </title>
+    <title> <?=$page_title?> | La Clairière </title>
 </head>
 <body>
     <header>
