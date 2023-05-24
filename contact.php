260391ab1bf8cdc693e49c900949e3bdf71af882
Romain9685 <rom1.grevrand@gmail.com>
2023-05-24 11:06:08 +0100

    contact + view model liaison

--- contact.php
+++ contact.php
@@ -10,4 +10,11 @@ if (!empty($_SESSION['message'])) {
 
 $page_title = 'Contact';
 
-require_once 'databaseConnection.php';
+ob_start();
+require_once 'app/model/databaseConnection.php';
+require_once 'app/model/contact.model.php';
+require_once 'app/view/contact.view.php';
+
+$content = ob_get_clean();
+
+require_once 'app/view/common/layout.php';
