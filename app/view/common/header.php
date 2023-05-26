a13de4c2094c97b9b56e3b9308a48a6686db90f6
nawalgb <gherbibahianawel@gmail.com>
2023-05-26 22:09:36 +0200

    correction liens menu

--- app/view/common/header.php
+++ app/view/common/header.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> <?=$page_title?> | La Clairière </title>
+    <title> <?= $page_title ?> | La Clairière </title>
 </head>
+
 <body>
     <header>
         <ul>
-            <li> <a href = "#"> <img src ="public/images/logos/La Clairièer 48b.svg"></a></li>
-            <li> <a href = "#">L'entreprise</a></li>
-            <li> <a href = "#">Nos bières</a></li>
-            <li> <a href = "#">Commande</a></li>
-            <li> <a href = "#">Contact</a></li>
+            <li> <a href="#"> <img src="public/images/logos/La Clairièer 48b.svg"></a></li>
+            <li> <a href="#">L'entreprise</a></li>
+            <li> <a href="catalogue.php">Nos bières</a></li>
+            <li> <a href="commande.php">Commande</a></li>
+            <li> <a href="contact.php">Contact</a></li>
         </ul>
     </header>
     <main>
