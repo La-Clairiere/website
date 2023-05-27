388d337a4dfdff8f0837819bde4b4b7676457e4e
chattigre <admin@chattigre.fr>
2023-05-27 20:37:42 +0200

    Style

--- app/view/common/footer.php
+++ app/view/common/footer.php
@@ -1,10 +1,12 @@
 </main>
 <footer>
-    <ul>
-        <li> <a href="#">Mentions légales</a></li>
-        <li> <a href="#">Politique de confidentialité</a></li>
-        <li> <a href = ""><img src ="public/images/logos/Logo-insta-nb.png"></a><li>
-    </ul>
+    <nav>
+        <ul>
+            <li> <a href="#">Mentions légales</a></li>
+            <li> <a href="#">Politique de confidentialité</a></li>
+            <li> <a href = ""><img id="footer_insta_logo" src ="public/images/logos/Logo-insta-nb.png"></a><li>
+        </ul>
+    </nav>
 </footer>
 </body>
 </html>
--- app/view/common/header.php
+++ app/view/common/header.php
@@ -11,12 +11,18 @@
 
 <body>
     <header>
-        <ul>
-            <li> <a href="accueil.php"> <img src="public/images/logos/la_clairiere_48b.svg"></a></li>
-            <li> <a href="#">L'entreprise</a></li>
-            <li> <a href="catalogue.php">Nos bières</a></li>
-            <li> <a href="commande.php">Commander</a></li>
-            <li> <a href="contact.php">Contact</a></li>
-        </ul>
+        <nav id="logo_nav">
+            <ul>
+                <li> <a href="accueil.php"><img id="header_logo" src="public/images/logos/la_clairiere_48b.svg"></a></li>
+            </ul>
+        </nav>
+        <nav id="menu_nav">
+            <ul>
+                <li> <a href="#">L'entreprise</a></li>
+                <li> <a href="catalogue.php">Nos bières</a></li>
+                <li> <a href="commande.php">Commander</a></li>
+                <li> <a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
     </header>
     <main>
