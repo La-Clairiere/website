6bba7c01d831eb42180d9578dd18f582e146b98d
chattigre <admin@chattigre.fr>
2023-05-27 19:26:44 +0200

    Renaming logos files

--- app/view/common/header.php
+++ app/view/common/header.php
@@ -6,15 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> <?= $page_title ?> | La Clairière </title>
+    <link rel="stylesheet" href="public/css/main.css">
 </head>
 
 <body>
     <header>
         <ul>
-            <li> <a href="#"> <img src="public/images/logos/La Clairièer 48b.svg"></a></li>
+            <li> <a href="accueil.php"> <img src="public/images/logos/la_clairiere_48b.svg"></a></li>
             <li> <a href="#">L'entreprise</a></li>
             <li> <a href="catalogue.php">Nos bières</a></li>
-            <li> <a href="commande.php">Commande</a></li>
+            <li> <a href="commande.php">Commander</a></li>
             <li> <a href="contact.php">Contact</a></li>
         </ul>
     </header>
