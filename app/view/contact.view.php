64840c8a788fab789c6dcbdfe6c3eef01565848d
chattigre <admin@chattigre.fr>
2023-05-28 00:59:04 +0200

    Style
    Co-Authored-By: Romain9685 <131149678+Romain9685@users.noreply.github.com>

--- app/view/contact.view.php
+++ app/view/contact.view.php
@@ -1,9 +1,12 @@
-<form method="POST">
-<label>Nom : <input type="text" name="nom" required/> </label>
-<label>Prénom : <input type="text" name="prenom" required/> </label>
-<label>Adresse mail : <input type="email" name="mail"/></label>
-<label>objet du mail : <input type="text" name="objet"/></label>
-<label> Message : <textarea name="message"></textarea></label>
-<button type="submit" name="envoyer">Envoyer</button>
-</form>
-
+<div class="page-content">
+    <h1>Nous contacter</h1>
+    <p>Une question ? Une demande ? Contactez-nous via ce formulaire et nous vous répondrons au plus vite par e-mail !</p>
+    <form method="POST">
+        <div><label>Nom : <input type="text" name="nom" required/> </label></div>
+        <div><label>Prénom : <input type="text" name="prenom" required/> </label></div>
+        <div><label>Adresse mail : <input type="email" name="mail"/></label></div>
+        <div><label>Objet du mail : <input type="text" name="objet"/></label></div>
+        <div><label for="message"> Message : </label><br/><textarea name="message" id="message" style="width:300px;max-width: 100%;" rows="10"></textarea></div>
+        <div><button type="submit" name="envoyer">Envoyer</button></div>
+    </form>
+</div>
