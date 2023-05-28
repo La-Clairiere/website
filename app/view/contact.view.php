b2385a7fed19847e1223e0f354a01bde1548506e
chattigre <admin@chattigre.fr>
2023-05-28 19:29:42 +0200

    Layout update
    Co-Authored-By: Romain9685 <131149678+Romain9685@users.noreply.github.com>

--- app/view/contact.view.php
+++ app/view/contact.view.php
@@ -1,12 +1,17 @@
 <div class="page-content">
     <h1>Nous contacter</h1>
     <p>Une question ? Une demande ? Contactez-nous via ce formulaire et nous vous répondrons au plus vite par e-mail !</p>
+    <p>Vous souhaitez passer commande ? Rendez-vous plutôt sur notre <a href="commande.php">page de commande</a> dédiée.</p>
     <form method="POST">
-        <div><label>Nom : <input type="text" name="nom" required/> </label></div>
-        <div><label>Prénom : <input type="text" name="prenom" required/> </label></div>
-        <div><label>Adresse mail : <input type="email" name="mail"/></label></div>
-        <div><label>Objet du mail : <input type="text" name="objet"/></label></div>
-        <div><label for="message"> Message : </label><br/><textarea name="message" id="message" style="width:300px;max-width: 100%;" rows="10"></textarea></div>
-        <div><button type="submit" name="envoyer">Envoyer</button></div>
+        <fieldset>
+            <legend>Formulaire de contact</legend>
+            <label for="i_nom">Nom :</label> <input type="text" name="nom" id="i_nom" required/>
+            <label for="i_prenom">Prénom :</label> <input type="text" name="prenom" id="i_prenom" required/>
+            <label for="i_mail">Adresse mail :</label> <input type="email" name="mail" id="i_mail"/>
+            <label for="i_objet">Objet du mail :</label> <input type="text" name="objet" id="i_objet"/>
+            <label for="i_message"> Message :</label>
+            <textarea name="message" id="i_message" style="max-width: 100%;" rows="15"></textarea>
+            <button type="submit" name="envoyer">Envoyer</button>
+        </fieldset>
     </form>
 </div>
