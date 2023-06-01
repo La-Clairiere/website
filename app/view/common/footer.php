95d3aee068824e7e947f7b8a9596e59801657074
Romain9685 <rom1.grevrand@gmail.com>
2023-06-01 22:48:23 +0100

    Re update footer

--- app/view/common/footer.php
+++ app/view/common/footer.php
@@ -2,10 +2,19 @@
 <footer>
     <nav>
         <ul>
-            <li> <a href="#">Mentions légales</a></li>
-            <li> <a href="#">Politique de confidentialité</a></li>
-            <li> <a href = "https://www.instagram.com/laclairiere_m/"><img id="footer_insta_logo" src ="public/images/logos/Logo-insta-nb.png"></a><li>
-            <li> <a href=""><img id="footer_twitter_logo" src="public/images/logos/Logo-twitter.png"></a></li>
+            <li> <a href="mentionslegales.php">Mentions légales</a></li>
+        </ul>
+        <ul>
+            <li><a href="https://www.instagram.com/laclairiere_m/"><img id="footer_insta_logo" src ="public/images/logos/Logo-insta-nb.png"></a></li>
+            <li> <a href="https://twitter.com/laclairiere_m"><img id="footer_twitter_logo" src="public/images/logos/Logo-twitter.png"></a></li>
+        </ul>
+    </nav>
+    <nav id="menu_footer">
+        <ul>
+            <li> <a href="notrehistoire.php">L'entreprise</a></li>
+            <li> <a href="catalogue.php">Nos bières</a></li>
+            <li> <a href="commande.php">Commander</a></li>
+            <li> <a href="contact.php">Contact</a></li>
         </ul>
     </nav>
 </footer>
