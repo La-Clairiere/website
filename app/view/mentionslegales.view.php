9a0f7db43eeba07d59c803dc7a75172fcb2733d6
chattigre <admin@chattigre.fr>
2023-06-02 20:43:09 +0200

    Creation page mentions legales

---
+++ app/view/mentionslegales.view.php
@@ -0,0 +1,23 @@
+<div class="page-content">
+    <h1>Mentions légales</h1>
+    <div style="display:flex; flex-wrap:wrap; flex-direction: row;">
+        <div>
+            <h2>&Eacute;diteur du site</h2>
+            <p><strong>La Clairière</strong></p>
+            <p>Brasserie étudiante semi-fictive</p>
+            <p>IUT Sénart-Fontainebleau<br>
+            Batiment H<br>
+            240 rue de la Motte<br>
+            77550 Moissy-Cramayel</p>
+            <p style="color:red;font-weight:bold;">Les produits présentés sur ce site sont fictifs, non commercialisés et créés dans un objectif exclusivement pédagogique.</p>
+        </div>
+        <div>
+            <h2>Hébergeur</h2>
+            <p><strong>o2switch</strong></p>
+            <p>Chemin des Pardiaux<br>
+            63000 Clermont-Ferrand<br>
+            <a href="https://o2switch.fr/" rel="nofollow">https://o2switch.fr/</a>
+        </p>
+        </div>
+    </div>
+</div>
