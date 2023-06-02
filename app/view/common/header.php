8ee57509c371704363df5008ab01b8a63ccf7cb5
Romain9685 <rom1.grevrand@gmail.com>
2023-06-02 21:02:55 +0100

    favicon header

--- app/view/common/header.php
+++ app/view/common/header.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> <?= $page_title ?> | La Clairière </title>
     <link rel="stylesheet" href="public/css/main.css">
+    <link rel="icon" href="public/images/favicon.png">
 </head>
 
 <body>
