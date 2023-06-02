e5f3b99e6228dc0a169b411441388d85526f8100
cocotheboss <jostcoralie@gmail.com>
2023-06-02 15:25:54 +0200

    global config + nous.css

--- config.php
+++ config.php
@@ -1,7 +1,6 @@
 <?php 
-$config = [
-    "dbName"=>'laclairiere',
-    "dbUsername"=>'root',
-    "dbPassword"=>''
 
-];
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'laclairiere');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
