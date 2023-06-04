2cedf82c9ca5f6a4579edb824a61b42c89b79738
Nouurr2004 <halimnour342@gmail.com>
2023-06-05 01:00:51 +0200

    jsp

--- app/view/index.view.php
+++ app/view/index.view.php
@@ -1,4 +1,6 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+<link rel="stylesheet" href="acceuil.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
 <body>
       <div class="banner">
         <h1><span class="logo">La Clairière</span></h1>
@@ -14,76 +16,25 @@
       <div class="nosproduits">
         <h2><br><br>NOS PRODUITS</br></br></h2>
       </div>
-      <div class="catalog-page">
-    <div class="catalog-container" >
-      <div class="catalog-list">
+      <?php foreach ($catalogue as $biere) : ?>
         <div class="catalog-item">
-          <img src="images/axelmockup.png" alt="Bière 1" class="item-image">
-          <h3>La Blonde</h3>
-          <p>Une bière blonde où la douceur juteuse de la pêche rencontre les nuances florales de l'hibiscus.</p>
-          <button class="like-button">
-            <i class="far fa-heart"></i>
-          </button>
+            <src="public/images/bieres/bouteille_<?=$biere['ref_biere']?>.png" alt="<?=$biere['nom']?>">
+            <h3><?= $biere['nom'] ?></h3>
+            <p><?= $biere['description'] ?></p>
+            <button class="like-button">
+                <i class="far fa-heart"></i>
+            </button>
         </div>
-        <div class="catalog-item">
-          <img src="images/gracemockup.png" alt="Bière 2" class="item-image">
-          <h3>Gracieuse</h3>
-          <p>Délicatement parfumée à la fleur de cerisier, une bière florale et revigorante.</p>
-          <button class="like-button">
-            <i class="far fa-heart"></i>
-          </button>
-        </div>
-        <div class="catalog-item">
-          <img src="images/nourmockup.png" alt="Bière 3" class="item-image">
-          <h3>Vertueuse</h3>
-          <p>Une bière à la myrtille et à la violette, fruitée, florale et envoûtante.</p>
-          <button class="like-button">
-            <i class="far fa-heart"></i>
-          </button>
-        </div>
-        <div class="catalog-item">
-          <img src="images/romainmockup.png" alt="Bière 4" class="item-image">
-          <h3>Douceur Inégalée</h3>
-          <p>Un mariage subtil entre les arômes vibrants du jasmin et la douceur naturelle du miel.</p>
-          <button class="like-button">
-            <i class="far fa-heart"></i>
-          </button>
-        </div>
-        <div class="catalog-item">
-          <img src="images/coraliemockup.png" alt="Bière 5" class="item-image">
-          <h3>La Brune</h3>
-          <p>Une délicate harmonie de saveurs légères et rafraîchissantes. </p>
-          <button class="like-button">
-            <i class="far fa-heart"></i>
-          </button>
-        </div>
-        <div class="catalog-item">
-          <img src="images/luciemockup.png" alt="Bière 6" class="item-image">
-          <h3>La Blache</h3>
-          <p>Une bière blanche rafraîchissante, offrant une expérience légère et pétillante.</p>
-          <button class="like-button">
-            <i class="far fa-heart"></i>
-          </button>
-        </div>
-        <div class="catalog-item">
-          <img src="images/nawalmockup.png" alt="Bière 7" class="item-image">
-          <h3>Tropicale</h3>
-          <p>Une fusion entre l'arôme floral envoûtant de la rose avec la douceur exotique du litchi.</p>
-          <button class="like-button">
-            <i class="far fa-heart"></i>
-          </button>
-        </div>
-      </div>
-      <div class="fleche fleche-gauche">←</div>
-      <div class="fleche fleche-droite">→</div>
+    <?php endforeach; ?>
+</div>
+  <div class="container">
+    <div class="image"></div>
+    <div class="text">
+      <h1>Découvrez La Clairière,</h1>
+      <p> Une bière authentique et naturelle par excellence. Entre tradition et innovation plongez dans l'essence de la nature avec chaque gorgée. La Clairière – l’essence même.
+</p>
     </div>
-    <button class="commander-button">Commander</button>
   </div>
-  <div class="informations">
-    <img src="images/IMG_3567.PNG" alt="artisants" class = imageinfo>
-    <h3>Une histoire de bière..</h3>
-    <p>Il était une fois une histoire de  </p>
-
     <script> // JS du scroll du mini catalogue de biere
       const catalogList = document.querySelector('.catalog-list');
       const catalogItems = document.querySelectorAll('.catalog-item');
